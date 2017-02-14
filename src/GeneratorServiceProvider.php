3ed0c77bac436c7a98368ee1f442f25ca5fa9782
Linus Staf <email@linusstaf.com>
2017-02-14 19:18:44 +0100

    Add basic laravel provider.

---
+++ src/GeneratorServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+namespace Staf\Generator;
+
+use Illuminate\Support\ServiceProvider;
+
+class GeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Setup the config file for publishing.
+        $this->publishes([__DIR__ . '/config.php' => config_path('generator.php')], 'config');
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(Builder::class, function ($app) {
+            return new Builder([
+                'source_path' => $app['config']['generator']['source_path'],
+                'target_path' => $app['config']['generator']['source_path'],
+                'cache_path'  => $app['config']['generator']['source_path'],
+            ]);
+        });
+    }
+}
---
+++ src/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    // Set the directory where the view files for the static site are located.
+    'source_path' => '',
+
+    // Set the target directory we are going to build the site in.
+    'target_path' => '',
+
+    // Set the cache directory used by the Blade compiler
+    'cache_path'  => storage_path('framework/views'),
+
+];
